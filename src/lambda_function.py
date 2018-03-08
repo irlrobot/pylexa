9895672f49fa70050356078d98aee7b5d12f6473
Josh Campbell <joshcb@amazon.com>
2018-03-08 12:37:32 -0800

    renames main to lamba_function and updates the boilerplate

---
+++ src/lambda_function.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Skill Name
+v2.0.0
+github.com/irlrobot
+"""
+from __future__ import print_function
+import json
+import responses
+
+def lambda_handler(event, _context):
+    '''main function for AWS Lambda'''
+    print('=====lambda handler started...')
+    print(json.dumps(event))
+
+    if event['request']['type'] == "LaunchRequest":
+        return launch_request(event['request'], event['session'])
+    if event['request']['type'] == "IntentRequest":
+        return intent_request(event['request'], event['session'])
+    if event['request']['type'] == "SessionEndedRequest":
+        return end_session_request(event['request'], event['session'])
+
+def launch_request(request, session):
+    '''handles modal launches'''
+    print("=====launch request...")
+
+def intent_request(request, session):
+    '''route intent'''
+    intent = request['intent']
+    intent_name = request['intent']['name']
+    print("=====intent is: " + intent_name)
+    if intent_name == "BLAH":
+        print("=====BLAH intent...")
+        return blah(intent, session)
+
+def end_session_request(request, session):
+    '''handles stop/exit/quit'''
+    print("=====end session request...")
+    responses.speech("Goodbye", True, "")
