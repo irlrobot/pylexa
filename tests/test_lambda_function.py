1fd65980f2e9e6634edb266553d7fdc0eb963040
Josh Campbell <jcampbell84@gmail.com>
2018-05-06 07:40:38 -0700

    initial commit

---
+++ tests/test_lambda_function.py
@@ -0,0 +1,22 @@
+from src.lambda_function import (
+    lambda_handler,
+    launch_request,
+    intent_request,
+    end_session_request
+)
+
+####################
+#lambda_handler
+####################
+
+####################
+#launch_request
+####################
+
+####################
+#intent_request
+####################
+
+####################
+#end_session_request
+####################
---
+++ tests/test_responses.py
@@ -0,0 +1,22 @@
+from src.responses import (
+    speech,
+    speech_with_card,
+    play_audio,
+    stop_audio
+)
+
+####################
+#speech
+####################
+
+####################
+#speech_with_card
+####################
+
+####################
+#play_audio
+####################
+
+####################
+#stop_audio
+####################
