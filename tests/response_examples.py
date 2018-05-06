464dd3b7f0f23589f588139b2fbc234d71ca095f
Josh Campbell <jcampbell84@gmail.com>
2018-05-06 09:59:48 -0700

    adds additional example responses

--- tests/response_examples.py
+++ tests/response_examples.py
@@ -21,3 +21,88 @@ def session_ended():
             }
         }
     }
+
+def speech_end_session():
+    '''example speech response with ending a session'''
+    return {
+        "version": "1.0",
+        "response": {
+            "shouldEndSession": True,
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": 'blah'
+            },
+            "reprompt": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": 'blorp'
+                }
+            }
+        }
+    }
+
+def speech_with_card_end_session():
+    '''example speech response with card with ending a session'''
+    return {
+        "version": "1.0",
+        "response": {
+            "shouldEndSession": True,
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": 'blah'
+            },
+            "reprompt": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": 'blorp'
+                }
+            },
+            "card": {
+                "type": "Standard",
+                "title": 'card_title',
+                "text": 'card_text',
+                "image": {
+                    "smallImageUrl": 'card_img_sm',
+                    "largeImageUrl": 'card_img_lg'
+                }
+            }
+        }
+    }
+
+def play_audio_example():
+    '''example for sending play audio directive'''
+    return {
+        "version": "1.0",
+        "sessionAttributes": {},
+        "response": {
+            "directives": [
+                {
+                    "type": "AudioPlayer.Play",
+                    "playBehavior": 'behavior',
+                    "audioItem": {
+                        "stream": {
+                            "token": "doesntreallymatter",
+                            "url": 'stream',
+                            "offsetInMilliseconds": 0
+                        }
+                    }
+                }
+            ],
+            "shouldEndSession": True
+        }
+    }
+
+def stop_audio_example():
+    '''example for sending stop audio directive'''
+    return {
+        "version": "1.0",
+        "sessionAttributes": {},
+        "response": {
+            "directives": [
+                {
+                    "type": "AudioPlayer.Stop",
+                }
+            ],
+            "shouldEndSession": True
+        }
+    }
