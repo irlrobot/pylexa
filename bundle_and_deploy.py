a95ef771a0e8e8e05d9f5a5df76a3824dd7647da
Josh Campbell <josh@p4a.net>
2020-07-22 12:46:56 -0700

    script will now combine src and deps dirs into a deployment bundle

--- bundle_and_deploy.py
+++ bundle_and_deploy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Bundle and deploy the Lambda function
-v2.0.0
+v2.1.0
 """
 import argparse
 import shutil
@@ -10,8 +10,10 @@ import boto3
 from botocore.exceptions import ClientError
 
 parser = argparse.ArgumentParser()
-parser.add_argument("function", help="The name of the AWS Lambda function to update.")
+parser.add_argument(
+    "function", help="The name of the AWS Lambda function to update.")
 args = parser.parse_args()
+
 
 def publish_new_version(artifact, func_name):
     """
@@ -39,10 +41,29 @@ def publish_new_version(artifact, func_name):
         print("Failed to access " + artifact + ".\n" + str(err))
         return False
 
+
+def create_bundle():
+    """ Create our deployment bundle """
+    try:
+        shutil.copytree("src", ".tmp_build", dirs_exist_ok=True)
+        shutil.copytree("deps", ".tmp_build", dirs_exist_ok=True)
+    except FileNotFoundError as err:
+        print("""
+        Error creating deployment bundle. 
+        Make sure both a 'src' and 'deps' directory exist.
+        """)
+        print(str(err))
+        return False
+
+    shutil.make_archive("lambda_function", "zip", ".tmp_build")
+    shutil.rmtree(".tmp_build")
+    return True
+
+
 def main():
     " Your favorite wrapper's favorite wrapper "
     print("Creating deployment package...")
-    shutil.make_archive("lambda_function", "zip", "src")
+    create_bundle()
 
     print("Starting deploy...")
     published_new_version = publish_new_version(
@@ -54,6 +75,7 @@ def main():
     else:
         print("Failed to deploy!")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
